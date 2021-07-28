300f35ac9f06d485d02bda2318c536875250550d
jasonandjay <342690199@qq.com>
2021-07-28 16:03:32 +0800

    手动实现MVVM-proxy+observe

---
+++ mvvm/mvvm.js
@@ -0,0 +1,33 @@
+import Observer from './observe.js';
+
+class MVVM{
+    /**
+     * @param $options
+     * $el 模版挂载的位置
+     * data MVVM实例驱动试图的数据 
+     */
+    constructor($options){
+        this._data = $options.data;
+        // 把_data内部的属性代理到this上
+        this._proxy(this._data);
+        // 通过observe劫持data
+        new Observer(this._data);
+
+        // 挂载$el
+    }
+    // 使用this.property代理this._data.property
+    _proxy(data){
+        for (let key in data){
+            Object.defineProperty(this, key, {
+                get(){
+                    return this._data[key]
+                },
+                set(newValue){
+                    this._data[key] = newValue;
+                }
+            })
+        }
+    }
+}
+
+export default MVVM;
